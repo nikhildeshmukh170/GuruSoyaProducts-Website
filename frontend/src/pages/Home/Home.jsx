46a9798b2c3d61554cc7cf3c634cb49a16c835e2
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-05-20 23:42:33 +0530

    2

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
+// import SectionBanner from "https://framer.com/m/Section-Banner-4gkX.js@ELteH7KJpwGrNHwRlIve"
 
 const Home = () => {
   return (
     <div>
         <Header />
+        {/* <SectionBanner /> */}
     </div>
   )
 }

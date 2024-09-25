08d49ea1dc9516872e5fd924a313b87272f053ff
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-09-26 04:30:06 +0530

    update url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://gurusoyaproducts-website.onrender.com/"
 
   return (
     <div>

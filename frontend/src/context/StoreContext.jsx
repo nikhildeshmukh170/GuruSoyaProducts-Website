8927f41751dd6b05667fbdfa77e65c9d5012ad62
Nikhil Deshmukh <115735153+nikhildeshmukh170@users.noreply.github.com>
2024-09-26 04:17:01 +0530

    deploy backend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const[cartItems, setCartItems] = useState({});
-    const url = "http://localhost:4000";
+    const url = "https://gurusoyaproducts-website.onrender.com/";
     const[token,setToken] = useState("");
     const [item_list,setItemList] = useState([]);
     
